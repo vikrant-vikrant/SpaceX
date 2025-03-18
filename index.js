5467374c0cfeee18e7bda5e02cacb70793798e51
vikrant-vikrant <123059176+vikrant-vikrant@users.noreply.github.com>
2025-03-18 12:15:36 +0530

    improve style

--- index.js
+++ index.js
@@ -5,43 +5,43 @@ const path = require("path");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 const ejsMate = require("ejs-mate");
-app.engine("ejs",ejsMate);
+app.engine("ejs", ejsMate);
 
-app.get("/home",(req,res)=>{
+app.get("/home", (req, res) => {
   res.render("listings/SpaceX.ejs");
 });
 
-app.get("/update",(req,res)=>{
+app.get("/update", (req, res) => {
   res.render("listings/update.ejs");
 });
 
-app.get("/falcon9",(req,res)=>{
+app.get("/falcon9", (req, res) => {
   res.render("listings/falcon9.ejs");
 });
-app.get("/falconheavy",(req,res)=>{
+app.get("/falconheavy", (req, res) => {
   res.render("listings/falconheavy.ejs");
 });
-app.get("/dragon",(req,res)=>{
+app.get("/dragon", (req, res) => {
   res.render("listings/dragon.ejs");
 });
-app.get("/starship",(req,res)=>{
+app.get("/starship", (req, res) => {
   res.render("listings/starship.ejs");
 });
-app.get("/humanspaceflight",(req,res)=>{
+app.get("/humanspaceflight", (req, res) => {
   res.render("listings/humanspaceflight.ejs");
 });
-app.get("/rideshare",(req,res)=>{
+app.get("/rideshare", (req, res) => {
   res.render("listings/rideshare.ejs");
 });
-app.get("/starshield",(req,res)=>{
+app.get("/starshield", (req, res) => {
   res.render("listings/starshield.ejs");
 });
-app.get("/starlink",(req,res)=>{
+app.get("/starlink", (req, res) => {
   res.render("listings/starlink.ejs");
 });
 
-app.listen(8080,()=>{
-  console.log('App is listing to port : 8080');
-})
+app.listen(8080, () => {
+  console.log("App is listing to port : 8080");
+});
